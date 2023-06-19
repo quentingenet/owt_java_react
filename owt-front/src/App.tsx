93846d650a7732e9ae8f8f15419a489c453c62da
Quentin Genet <genetquentin@gmail.com>
2023-06-19 21:13:19 +0200

    IMplmenting Landpage

--- owt-front/src/App.tsx
+++ owt-front/src/App.tsx
@@ -2,11 +2,17 @@ import './App.css';
 import NavbarConnected from './components/NavbarConnected/NavbarConnected.tsx';
 import Navbar from './components/Navbar/Navbar.tsx';
 import { useUserContext } from './contexts/UserContext.tsx';
+import Landpage from './pages/Landpage/Landpage.tsx';
 
 function App() {
 	const userContext = useUserContext();
 
-	return <>{userContext.isUserLoggedIn ? <NavbarConnected /> : <Navbar />}</>;
+	return (
+		<>
+			{userContext.isUserLoggedIn ? <NavbarConnected /> : <Navbar />}
+			<Landpage />
+		</>
+	);
 }
 
 export default App;
