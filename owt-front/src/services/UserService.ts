569e585b99eb9d349c3c0bcd3fee5930ab51db21
Quentin Genet <genetquentin@gmail.com>
2023-07-05 20:40:44 +0200

    Login component : API test connexion with fetch request

---
+++ owt-front/src/services/UserService.ts
@@ -0,0 +1,25 @@
+import { ILoginForm } from "../models/ILoginForm";
+
+export const login = (data: ILoginForm) => {
+    const requestData = {appUsername: data.username, password: data.password}
+    fetch('http://localhost:7777/api/v1/login',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestData)
+  })
+    .then(response => {if (response.ok) {
+        const headers = response.headers;
+        const jwt = headers.get('Authorization');
+        console.log('JWT :', jwt);
+        return response.json();
+      } else {
+        throw new Error('Erreur lors de la requête à l\'API');
+      }
+    })
+    .catch(error => {
+
+      console.error('Erreur lors de l\'appel à l\'API :', error);
+    });
+};
