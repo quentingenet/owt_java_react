a41c9a8d725d49ffa1d894c70bce139920b1d680
Quentin Genet <genetquentin@gmail.com>
2023-07-05 18:49:24 +0200

    first components login and register

---
+++ owt-front/src/models/IRegisterForm.ts
@@ -0,0 +1,12 @@
+export interface IRegisterForm{
+    username: string;
+    password: string;
+    passwordBis: string;
+    emailUser: string;
+    yearBirth: number;
+    isMale: boolean;
+    isEuropeanUnitMeasure: boolean;
+    bodySize: number;
+    goalWeight: number;
+
+}
